9bb3d788bdf212e9f1e47046bba03ef000a39699
aayushsing1 <111671884+aayushsing1@users.noreply.github.com>
2022-08-21 10:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route("/hello")
-def index():
-    return render_template("index.html")
-
-class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.title}"
-
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
@@ -62,7 +49,14 @@ def delete(sno):
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
 
 if __name__ == "__main__":
     app.run(debug=True)
